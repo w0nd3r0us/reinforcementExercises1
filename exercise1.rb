acfeb27307e594eccc43d1e791a70929ef0e5a7d
w0nd3r0us <steven.grewal@gmail.com>
2018-05-23 14:40:27 -0400

    half way through first problem

---
+++ exercise1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby -wKU
+documentary = "Hadwin's Judgement"
+drama = "The Notebook"
+comedy = "IT Crowd"
+dramedy = "Me and Earl and the Dying Girl"
+book = "Zen Bhudism and the art of motorcycle maintenance"
+
+
+puts "Do you enjoy documentaries (yes/no)"
+input_documentary = gets.chomp
+
+puts "Do you enjoy dramas (yes/no)"
+input_drama = gets.chomp
+
+puts "Do you enjoy comedies (yes/no)"
+input_comedy = gets.chomp
