013fdca7f9cc0c15dce6ff1eca506a63afbbb6c1
w0nd3r0us <steven.grewal@gmail.com>
2018-05-23 14:51:23 -0400

    finished first task

--- exercise1.rb
+++ exercise1.rb
@@ -15,14 +15,14 @@ input_drama = gets.chomp
 puts "Do you enjoy comedies (yes/no)"
 input_comedy = gets.chomp
 
-if input_documentary == "yes" && input_drama == "no" && input_comedy "no"
+if input_documentary == "yes" && input_drama == "no" && input_comedy == "no"
   puts "You should watch #{documentary}."
-elsif input_documentary == "no" && input_drama == "yes" && input_comedy "yes"
+elsif input_documentary == "no" && input_drama == "yes" && input_comedy == "yes"
   puts "You should watch #{dramedy}."
-elsif input_documentary == "no" && input_drama == "yes" && input_comedy "no"
+elsif input_documentary == "no" && input_drama == "yes" && input_comedy == "no"
   puts "You should watch #{drama}."
-elsif input_documentary == "no" && input_drama == "no" && input_comedy "yes"
+elsif input_documentary == "no" && input_drama == "no" && input_comedy == "yes"
   puts "You should watch #{comedy}."
-elsif input_documentary == "no" && input_drama == "no" && input_comedy "no"
+elsif input_documentary == "no" && input_drama == "no" && input_comedy == "no"
   puts "You should read #{book} instead."
 end
